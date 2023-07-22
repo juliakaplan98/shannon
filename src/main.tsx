1c052321b4c73a3a0afd2fb28540d5617b877fa0
Julia Kaplan <juliakaplan98@outlook.com>
2023-07-22 16:00:49 -0400

    Add pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import Layout from './pages/layout';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Blogs from "./pages/blogs";
+import Contact from "./pages/contact";
+import NoPage from "./pages/nopage";
 
 export const Main = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Layout />}>
-
+                    <Route index element={<Home />} />
+                    <Route path="blogs" element={<Blogs />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NoPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
